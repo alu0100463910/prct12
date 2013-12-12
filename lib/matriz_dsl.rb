ea52e82b1d07c9b71a37839cf90133fc8776890e
Fabio Mendoza Bello <alu0100463910@ull.edu.es>
2013-12-12 23:10:42 +0000

    Mejorando initialize y definido nombre d funciones

--- lib/matriz_dsl.rb
+++ lib/matriz_dsl.rb
@@ -1,17 +1,31 @@
 class MatrixDSL < Matrix
 	def initialize(op)
-		@operando = op.to_s
-		case op
-		when "suma"
-			@operando= "+"
-		when "resta"
-			@operando = "-"
-		when "multiplica"
-			@operando = "*"
-		else
-			puts "Operando desconocido"
+		case op.to_s
+			when "suma"
+				@operando= "+"
+			when "resta"
+				@operando = "-"
+			when "multiplica"
+				@operando = "*"
+			else
+				puts "Operando desconocido"
 		end
-		@matrixA = nil
-		@matrixB = nil
+		@matriz1 = nil
+		@matriz2 = nil
 	end
+	
+	#Definir  tipo de matriz dispersa o densa
+	def option(op)
+		
+	end
+
+        # Se definen todos las operaciones con matrices
+        def operand(other) 
+        end
+                
+        # Funcion ejecucion del DSL
+        def execute
+		#falta
+                        
+        end
 end
