24b154ab8c3d1b98b08179571ee8ebfff1f0f560
Fabio Mendoza Bello <alu0100463910@ull.edu.es>
2013-12-13 15:16:03 +0000

    Añadimos clase matrizdsl dentro del modulo Matriz_sf

--- lib/matriz_sf.rb
+++ lib/matriz_sf.rb
@@ -282,6 +282,55 @@ module Matriz_sf
             [Fraccion.new(o,1.0),self]
         end
     end
+    
+	class MatrixDSL < Matrix
+		#Se le pasa el tipo de operando que se va quiere usar
+		def initialize (op)
+			@op = op.to_s
+			case @op
+				when "suma"
+					@operando = "+"
+				when "resta"
+					@operando = "-"
+				when "multiplicacion"
+					@operando = "*"
+				else
+					puts "Operando desconocido"
+			end
+			@matriz1 = nil
+			@matriz2 = nil
+		end
+		
+		#Definir  tipo de matriz dispersa o densa
+		def option (opcion)
+			@tipo = opcion
+		end
+
+		# Se definen todos las operaciones con matrices
+		def operand (o)
+			if @matriz1 == nil
+				@matriz1 == o
+			else
+				@matriz2 == o
+			end
+			execute
+		end
+			
+		# Funcion de ejecucion del DSL
+		def execute
+			
+			if @matriz1 != nil and @matriz2 != nil
+				@m1 = "Matriz " + @tipo.to_s + ".new(@matriz1)"
+				puts "Matriz 1: #{@m1}"
+				@m2 = "Matriz" + @tipo.to_s + ".new(@matriz2)"
+				puts "Matriz 2: #{@m2}"
+				r = @m1.to_s + "." + @operando.to_s + "(" + " " + @m2.to_s + ")"
+				r = eval(r)
+				puts @m1.to_s +" " + @op.to_s + " "+ @m2.to_s + "=    #{r}"
+			end
+				
+		end
+	end
 end
 
 
